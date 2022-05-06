9af7f2b7b707bb25ebfce0933e9879230680cec7
Zakella <zapolski.slava@gmail.com>
2022-05-06 16:49:48 +0300

    курс валют 1.1

--- parcing/VS/rates.py
+++ parcing/VS/rates.py
@@ -1,15 +1,41 @@
 import requests
 import json
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 responce = requests.get("http://pki.maib.md/rates/")
 # print(responce.text)
 
 
-root = BeautifulSoup (responce.text, 'html.parser')
-root.find_all('br',responce.text)
-for i in root:
+soup = BeautifulSoup(responce.content, "html.parser")
+print()
+
+data = soup.find('pre')
+# data = soup.find(href=True)
+# print(data)
+
+list_rates = {}
+
+for i in data:
+    # tag = i.br
+    # print(tag)
+    # print()
+    print(type(i))
+    if isinstance(i, element.NavigableString):
+        print(i.string.split()[0])
+        # list_rates[]
+    # if not i == "<br/":
+    #     print(i)
     print(i)
+    # d2 = i.find("br")
+    # print(d2)
+
+    # print(i, type(i))
+    # print(i.attrs.findall("a"))
+
+# root.find_all('pre')
+# for i in root:
+#     print(i)
+#     break
 # print(root)
 # sup = BeautifulSoup.find_all(responce.text, "b")
 # a = responce.text.find("5/6/2022")
