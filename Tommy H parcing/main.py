25b0e34fa07b81032b8848e52f1e984d6fbdc5b0
Zakella <zapolski.slava@gmail.com>
2022-05-17 15:15:54 +0300

    парсинг сайта, добавление номенклатуры и цены

--- Tommy H parcing/main.py
+++ Tommy H parcing/main.py
@@ -1,33 +1,58 @@
 import requests
 import json
-from fake_useragent import UserAgent
-import mechanize
+from bs4 import BeautifulSoup
 
 
 def parce():
-
     with open("headers.json") as file:
         headers = json.load(file)
         print(headers)
+    #
+    # url = "https://usa.tommy.com/en/tommy-hilfiger-sale"
+    # response = requests.session().get(url=url, headers=headers)
+    # with open("index.html", "w") as file:
+    #     file.write(response.text)
+    #
+    # print(response.text)
 
+    with open("index.html") as file:
+        soup = BeautifulSoup(file.read(), "lxml")
+        all_refs = soup.find_all(class_="productImage focusParent")
+        url_list = []
+        for item in all_refs:
+            products = item.find_all(class_="productThumbnail")
+            for value in products:
+                url_list.append(value.get("href"))
 
+    for item in url_list:
+        print(item)
 
+        # print(item)
+        # response = requests.session().get(url=item.get("url"), headers=headers)
+        # with open("index_inner.html", "w") as file:
+        #     file.write(response.text)
+        item_dict = {}
+        with open("index_inner.html") as file:
+            soup = BeautifulSoup(file.read(), "lxml")
+            data = soup.find_all(class_="productView")
+            for value in data:
+                item_dict["name"] = "".join([item.text for item in value.find_all(class_="productNameInner")])
+                prices = value.find_all(class_="namePartPriceContainer")
+                for price in prices:
+                    print(price)
+                # for b in a:
+                #     print(b.text)
+                # print(value.find_all(class_= "productNameInner").get("itemprop"))
+                # item_dict["name"] = value.
+                # print(value)
 
+        break
 
-    url = "https://usa.tommy.com/en/tommy-hilfiger-sale"
-    responce = requests.session().get(url=url, headers=headers)
-    print(responce.json())
-
-    # br = mechanize.Browser()
-    # br.open(url)
-    # # follow second link with element text matching regular expression
-    # response1 = br.follow_link(text_regex=r"cheese\s*shop", nr=1)
-    # print(response1.read())
+        # print(f"Total items in page {i}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parce()
 
-
-
+# https://usa.tommy.com/ProductListingView?includeLowStockIndicator=true&catalogId=10551&isHomeDepartment=false&pageSize=30&disableProductCompare=true&langId=-1&storeId=10151&categoryId=3074457345617404775&currentCategoryIdentifier=UNISEX_SALE_HID&imageBadgeIgnoreCategoryCodeParam=UNISEX_SALE_HID&beginIndex=30&minFacetCount=1&colorfacetselected=false&cache=true
